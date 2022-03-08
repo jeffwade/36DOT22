3d1d1f5e900456caae3471b0b87708f787201b21
Jeff Wade <jeffwade@users.noreply.github.com>
2022-03-07 16:27:26 -0800

    add Latest page

---
+++ src/components/plasmic/36_dot_22/icons/PlasmicIcon__Thirtysix.jsx
@@ -0,0 +1,42 @@
+// @ts-nocheck
+/* eslint-disable */
+/* tslint:disable */
+/* prettier-ignore-start */
+import React from "react";
+import { classNames } from "@plasmicapp/react-web";
+
+export function ThirtysixIcon(props) {
+  const { className, style, title, ...restProps } = props;
+  return (
+    <svg
+      xmlns={"http://www.w3.org/2000/svg"}
+      fill={"none"}
+      viewBox={"0 0 16 16"}
+      height={"1em"}
+      width={"1em"}
+      style={{
+        fill: "currentcolor",
+        ...(style || {}),
+      }}
+      className={classNames("plasmic-default__svg", className)}
+      {...restProps}
+    >
+      {title && <title>{title}</title>}
+
+      <path
+        d={"M0 5V0h7v16H0v-5h3v2h1V9H1V6h3V3H3v2H0z"}
+        fill={"currentColor"}
+      ></path>
+
+      <path
+        fillRule={"evenodd"}
+        clipRule={"evenodd"}
+        d={"M15 0v5h-3V3h-1v3h4v10H8V0h7zm-4 9h1v4h-1V9z"}
+        fill={"currentColor"}
+      ></path>
+    </svg>
+  );
+}
+
+export default ThirtysixIcon;
+/* prettier-ignore-end */
