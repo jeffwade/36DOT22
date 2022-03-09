535849fbc0ae9b81ba55da38bd7638fa4aed3098
Jeff Wade <jeffwade@users.noreply.github.com>
2022-03-08 17:33:29 -0800

    :wrench: update gatsby deploy settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,21 +1,23 @@
 module.exports = {
-    siteMetadata: {
-        siteUrl: `https://www.yourdomain.tld`,
+  siteMetadata: {
+    siteUrl: `https://jeffdo.es/36dot22`,
+  },
+  pathPrefix: "/36dot22",
+  plugins: [
+    {
+      resolve: "@plasmicapp/loader-gatsby",
+      options: {
+        projects: [
+          {
+            id: "byT51ah2LGWp7TY4NhRWN5", 
+            token:
+              "0SU3BvNIg91GdE79QY1Z4hbcssU4me7rDwmjrJHT2ypmKClyIOYoMLOYlJyNro9HPLhwWgzb5fJFCxgpUUA", 
+          },
+        ],
+        // Fetches the latest revisions, whether or not they were unpublished!
+        // Disable for production to ensure you render only published changes.
+        preview: true,
+      },
     },
-    plugins: [
-      {
-        resolve: '@plasmicapp/loader-gatsby',
-        options: {
-          projects: [
-            {
-              id: 'byT51ah2LGWp7TY4NhRWN5', // ID of a project you are using
-              token: '0SU3BvNIg91GdE79QY1Z4hbcssU4me7rDwmjrJHT2ypmKClyIOYoMLOYlJyNro9HPLhwWgzb5fJFCxgpUUA' // API token for that project
-            }
-          ],
-          // Fetches the latest revisions, whether or not they were unpublished!
-          // Disable for production to ensure you render only published changes.
-          preview: true
-        }
-      }
-    ]
-}
+  ],
+};
